a70db33d7063d9c66cce1ee278625fed48081c17
Luqman Musah <musahluqman@gmail.com>
2021-08-07 00:21:25 +0000

    Add function getScore

--- src/request.js
+++ src/request.js
@@ -1,4 +1,25 @@
 const request = new XMLHttpRequest();
 const requestURL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/aQeQLOEMph5dw2BhyuYH/scores/'
 
-
+export default class Request {
+    constructor() {
+      this.scoreList = [];
+    }
+  
+    async getScore() {
+      const promise = new Promise((myResolve) => {
+        request.open('GET', requestURL);
+        request.onload = () => {
+          if (request.status === 200) {
+            myResolve(request.response);
+          } else {
+            myResolve('Error');
+          }
+        };
+        request.send();
+      });
+      const list = JSON.parse(await promise);
+      this.scoreList = list.result;
+      this.scoreList.sort((a, b) => b.score - a.score);
+    }
+  }
